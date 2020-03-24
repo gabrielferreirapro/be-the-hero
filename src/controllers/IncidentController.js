9e09ecfa4d7eb5efe5138149f7ae8d07a0396b8a
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 20:43:12 -0300

    Creating IncidentController and create method.

---
+++ src/controllers/IncidentController.js
@@ -0,0 +1,17 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async create(req, res) {
+    const { title, description, value } = req.body;
+    const ngo_id = req.headers.authorization;
+
+    const [id] = await connection("incident").insert({
+      title,
+      description,
+      value,
+      ngo_id
+    });
+
+    return res.json({ id });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const NgoController = require("./controllers/NgoController");
+const IncidentController = require("./controllers/IncidentController");
 const routes = express.Router();
 
 routes.get("/ngo", NgoController.index);
 routes.post("/ngo", NgoController.create);
+
+routes.post("/incident", IncidentController.create);
 
 module.exports = routes;
