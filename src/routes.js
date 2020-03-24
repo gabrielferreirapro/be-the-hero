94011611e170f96c6cb43cffff6e2a2e89946b23
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 20:23:54 -0300

    Listing all NGO's

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,11 @@ const connection = require("./database/connection");
 
 const routes = express.Router();
 
+routes.get("/ngo", async (req, res) => {
+  const ngos = await connection("ngo").select("*");
+  return res.json(ngos);
+});
+
 routes.post("/ngo", async (req, res) => {
   const { name, email, whatsapp, city, state } = req.body;
 
