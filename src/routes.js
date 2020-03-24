86708b089d155f2a4cb71fe00d57d98f1a0be5de
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 20:31:22 -0300

    Creating NgoController and refactoring routes to use controllers.

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,8 @@
 const express = require("express");
-const crypto = require("crypto");
-const connection = require("./database/connection");
-
+const NgoController = require("./controllers/NgoController");
 const routes = express.Router();
 
-routes.get("/ngo", async (req, res) => {
-  const ngos = await connection("ngo").select("*");
-  return res.json(ngos);
-});
-
-routes.post("/ngo", async (req, res) => {
-  const { name, email, whatsapp, city, state } = req.body;
-
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ngo").insert({ id, name, email, whatsapp, city, state });
-
-  return res.json({ id });
-});
+routes.get("/ngo", NgoController.index);
+routes.post("/ngo", NgoController.create);
 
 module.exports = routes;
