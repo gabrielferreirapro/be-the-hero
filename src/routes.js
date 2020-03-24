da4d2ea950f8f7d577202e539ea39772ebf33439
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 20:17:05 -0300

    Creating connection factory and saving new NGO's

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
+
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({
-    teste: "teste"
-  });
+routes.post("/ngo", async (req, res) => {
+  const { name, email, whatsapp, city, state } = req.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ngo").insert({ id, name, email, whatsapp, city, state });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
