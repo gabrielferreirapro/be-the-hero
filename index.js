edbcaef56f5d2c6e42d40b64e8c2d8997ead6b54
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 19:27:43 -0300

    Setting up nodemon and knex.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    return res.json({
-        teste :"teste"
-    })
-})
+// app.use(express.json())
+
+app.get("/", (req, res) => {
+  return res.json({
+    teste: "teste"
+  });
+});
 app.listen(3333);
