81a6681e7b33f5c3e240fa541a77827a946b38b1
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-24 21:21:27 -0300

    Listing specifics Incidents by NGO's

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,11 @@
+const connection = require("../database/connection");
+module.exports = {
+  async index(req, res) {
+    const ngo_id = req.headers.authorization;
+
+    const incidents = await connection("incident")
+      .where("ngo_id", ngo_id)
+      .select("*");
+    return res.json(incidents);
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const NgoController = require("./controllers/NgoController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+
 const routes = express.Router();
 
 routes.get("/ngo", NgoController.index);
@@ -9,5 +11,7 @@ routes.post("/ngo", NgoController.create);
 routes.get("/incident", IncidentController.index);
 routes.post("/incident", IncidentController.create);
 routes.delete("/incident/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
 
 module.exports = routes;
